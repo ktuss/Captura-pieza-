64778c6a9db77387d0889f043049b47cb5676790
ktuss <cuturrufoeduardo64@gmail.com>
2026-03-26 07:37:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -197,3 +197,11 @@ function startTimer(){
 /* BOTONES */
 function restart(){ startCamera(); }
 function resetGame(){ initGame(); }
+window.addEventListener("load", ()=>{
+
+  setTimeout(()=>{
+    document.getElementById("introAnimation").style.display="none";
+    document.getElementById("homeScreen").classList.add("active");
+  },2000);
+
+});
